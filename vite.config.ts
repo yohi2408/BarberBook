fef45b110a04724c6ea45cc26887373aaa7b38e6
yohi2408 <ditnum01@gmail.com>
2026-01-10 00:16:21 +0200

    Add real firebase config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Essential for GitHub Pages sub-folders like /BarberBook/
-  base: './',
+  // הגדרה מפורשת של התיקייה ב-GitHub Pages
+  base: '/BarberBook/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
